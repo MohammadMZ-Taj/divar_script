64f55c09d6218bd9f7c15099285abe296917bffa
mohsenrazavi <mohsenrazavi.self@gmail.com>
2023-07-23 18:59:51 +0330

    add requirements.txt

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import pandas as pd
 import os
 from bs4 import BeautifulSoup
 
-with open('constants.json' , 'r') as readconst:
+with open('constants.json', 'r') as readconst:
     const = json.load(readconst)
 with open('config.json', 'r', encoding='utf-8') as readconfig:
     config = json.load(readconfig)
 
 sent_messages = 1
+
 
 def get_district_code():
     out = []
@@ -23,17 +24,19 @@ def get_district_code():
             raise ValueError
     return out
 
-def min_max_value(min , max):
-    if max>min:
+
+def min_max_value(min, max):
+    if max > min:
         return {"max": max, "min": min}
     else:
         return {"min": min}
 
+
 def payload_json_schema():
-    out = { "category":{"value":"house-villa-rent"},
-            "sort":{"value":"sort_date"},
-            "cities":["6"]}
-    
+    out = {"category": {"value": "house-villa-rent"},
+           "sort": {"value": "sort_date"},
+           "cities": ["6"]}
+
     if config['houseconfig']['districts']:
         out['districts'] = {"vacancies": get_district_code()}
     if config['houseconfig']['credit']['max'] != 0 or config['houseconfig']['credit']['min'] != 0:
@@ -43,15 +46,17 @@ def payload_json_schema():
     if config['houseconfig']['size']['max'] != 0 or config['houseconfig']['size']['min'] != 0:
         out['size'] = min_max_value(config['houseconfig']['size']['min'], config['houseconfig']['size']['max'])
     if config['houseconfig']['rooms'] != "":
-        out['rooms'] = {'value' : config['houseconfig']['rooms']} 
-    
+        out['rooms'] = {'value': config['houseconfig']['rooms']}
+
     return out
 
+
 def min_max_value_string(min, max):
-    if max>min:
+    if max > min:
         return f"{min}-{max}"
     else:
         return f"{min}-"
+
 
 def rooms_to_eng(string):
     if string == 'بدون اتاق':
@@ -67,6 +72,7 @@ def rooms_to_eng(string):
     elif string == 'بیشتر':
         return 'more'
 
+
 def page_0_url():
     out = ""
     if config['houseconfig']['credit']['max'] != 0 or config['houseconfig']['credit']['min'] != 0:
@@ -81,30 +87,32 @@ def page_0_url():
         return out[:-1]
     return out
 
+
 def get_more_info(token):
     print(f'getting additional info for {token}')
-    out = {'land area' : '',
-           'area' : '',
-           'Year of construction' : ''}
+    out = {'land area': '',
+           'area': '',
+           'Year of construction': ''}
     for _ in range(5):
-        response = requests.get(const['page_url']+token)
+        response = requests.get(const['page_url'] + token)
         if response.status_code == 200:
             break
         time.sleep(1)
     soup = BeautifulSoup(response.content, "html.parser")
-    top_info = soup.find_all('div', class_ = 'kt-group-row-item kt-group-row-item--info-row')
+    top_info = soup.find_all('div', class_='kt-group-row-item kt-group-row-item--info-row')
     for item in top_info:
         subitems = item.find_all('span')
         if subitems[0].text == 'متراژ':
             out['area'] = subitems[1].text
         if subitems[0].text == 'ساخت':
             out['Year of construction'] = subitems[1].text
-    info = soup.find_all('div', class_ = 'kt-base-row kt-base-row--large kt-unexpandable-row')
+    info = soup.find_all('div', class_='kt-base-row kt-base-row--large kt-unexpandable-row')
     for item in info:
-        if item.find("p", class_ = 'kt-base-row__title kt-unexpandable-row__title').text == 'متراژ زمین':
-            out['land area'] = item.find("p", class_ = 'kt-unexpandable-row__value').text
+        if item.find("p", class_='kt-base-row__title kt-unexpandable-row__title').text == 'متراژ زمین':
+            out['land area'] = item.find("p", class_='kt-unexpandable-row__value').text
             break
     return out
+
 
 def get_data():
     print('getting data from divar')
@@ -112,17 +120,17 @@ def get_data():
     last_post_date = 0
     index = 0
     while True:
-        print('getting page : ',index)
+        print('getting page : ', index)
         if index > 0:
-            payload = {"page":index,
-                       "json_schema" : payload_json_schema()}
+            payload = {"page": index,
+                       "json_schema": payload_json_schema()}
             if last_post_date != 0:
                 payload['last-post-date'] = last_post_date
             json_payload = json.dumps(payload)
             header = {"Content-Type": "application/json"}
             response = requests.post(const['api_url'], data=json_payload, headers=header)
         else:
-            response = requests.get(const['api_url_page0']+page_0_url())
+            response = requests.get(const['api_url_page0'] + page_0_url())
         page = response.json()
         last_post_date = page['last_post_date']
         postlist = page['web_widgets']['post_list']
@@ -131,8 +139,10 @@ def get_data():
         for item in postlist:
             if item['widget_type'] != 'POST_ROW':
                 continue
-            data = {x:item['data'][x] for x in ['token', 'title', 'top_description_text', 'middle_description_text', 'bottom_description_text', 'image_count']}
-            if item['data']['image_count'] > 0 :
+            data = {x: item['data'][x] for x in
+                    ['token', 'title', 'top_description_text', 'middle_description_text', 'bottom_description_text',
+                     'image_count']}
+            if item['data']['image_count'] > 0:
                 data['image_url'] = item['data']['image_url'][1]['src']
             else:
                 # placeholder for image url cuz it was a list
@@ -143,19 +153,24 @@ def get_data():
     print(f'found {len(out)} items')
     return out
 
+
 def save_data(list):
     print('saving data...')
-    with open(config['save_file'] , "w")as csvfile:
+    with open(config['save_file'], "w") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(list[0])
         for item in list:
             writer.writerow(item.values())
 
+
 def create_save_if_not_exist():
     if not os.path.exists(config['save_file']):
         with open(config['save_file'], 'w') as createsave:
             createwriter = csv.writer(createsave)
-            createwriter.writerow(['token', 'title', 'top_description_text', 'middle_description_text', 'bottom_description_text', 'image_count', 'image_url'])
+            createwriter.writerow(
+                ['token', 'title', 'top_description_text', 'middle_description_text', 'bottom_description_text',
+                 'image_count', 'image_url'])
+
 
 def get_data_diffrence(data):
     data1 = pd.DataFrame(data)
@@ -163,23 +178,25 @@ def get_data_diffrence(data):
     result1 = data1[~data1['token'].isin(data2['token'])]
     return result1.values.tolist()
 
+
 def house_info(data):
     text = f"**{data[1]}**\n\n{data[2]}\n{data[3]}\n{data[4]}\n"
     if int(data[5]) > 0:
         text += f"تعداد عکس : {data[5]}\n"
     if data[7] != "":
-        text +=f"متراژ زمین : {data[7]}\n"
+        text += f"متراژ زمین : {data[7]}\n"
     if data[8] != "":
-        text +=f"متراژ خانه : {data[8]}\n"
+        text += f"متراژ خانه : {data[8]}\n"
     if data[9] != "":
-        text +=f"سال ساخت : {data[9]}\n"
+        text += f"سال ساخت : {data[9]}\n"
     text += f"\n{const['page_url']}{data[0]}"
     return text
-    
+
+
 def send_message(chat_id, text):
     for i in range(5):
         url = f"https://api.telegram.org/bot{config['bot_api_key']}/sendMessage"
-        payload = {"chat_id" : chat_id, "text" : text}
+        payload = {"chat_id": chat_id, "text": text}
         json_payload = json.dumps(payload)
         header = {"Content-Type": "application/json"}
         try:
@@ -189,11 +206,12 @@ def send_message(chat_id, text):
         except Exception:
             pass
     return False
+
 
 def send_photo(chat_id, photo_url, caption):
     for i in range(5):
         url = f"https://api.telegram.org/bot{config['bot_api_key']}/sendPhoto"
-        payload = {"chat_id" : chat_id, "photo" : photo_url, "caption" : caption}
+        payload = {"chat_id": chat_id, "photo": photo_url, "caption": caption}
         json_payload = json.dumps(payload)
         header = {"Content-Type": "application/json"}
         try:
@@ -205,10 +223,11 @@ def send_photo(chat_id, photo_url, caption):
 
     return False
 
+
 def save_not_sent(itemlist):
     print('saving not sent messages')
-    if len(itemlist)>0:
-        with open(config['not_sent_file'] , "w")as csvfile:
+    if len(itemlist) > 0:
+        with open(config['not_sent_file'], "w") as csvfile:
             writer = csv.writer(csvfile)
             for item in itemlist:
                 writer.writerow(item)
@@ -216,37 +235,41 @@ def save_not_sent(itemlist):
         if os.path.exists(config['not_sent_file']):
             os.remove(config['not_sent_file'])
 
+
 def notify_user(chat_id, row):
     global sent_messages
-    if sent_messages%20 == 0:
+    if sent_messages % 20 == 0:
         print('waitng for 60sec befor sending more messages')
         time.sleep(60)
     sent_messages += 1
     if int(row[5]) >= 1:
-        status= send_photo(chat_id, row[6], house_info(row))
+        status = send_photo(chat_id, row[6], house_info(row))
     else:
-        status= send_message(chat_id, house_info(row))
+        status = send_message(chat_id, house_info(row))
     return status
 
-def notify_all(chat_id,data):
+
+def notify_all(chat_id, data):
     print(f'sending {len(data)} items to user {chat_id}')
     not_sent = []
     for row in data:
-        sent = notify_user(chat_id , row)
+        sent = notify_user(chat_id, row)
         if not sent:
             print(f"coudnt sent to user {chat_id} this message :{row}")
             not_sent.append([chat_id] + row)
-    
+
     return not_sent
+
 
 def get_not_sent_data():
     out = []
     if os.path.exists(config['not_sent_file']):
-        with open(config['not_sent_file'] , "r")as csvfile:
+        with open(config['not_sent_file'], "r") as csvfile:
             reader = csv.reader(csvfile)
             for item in reader:
                 out.append(item)
     return out
+
 
 def main():
     not_sent_data = []
@@ -260,5 +283,6 @@ def main():
     save_not_sent(not_sent_data)
     save_data(data)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
