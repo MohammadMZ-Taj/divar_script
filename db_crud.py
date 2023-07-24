7f93c86288235137225bbfb512345b2276547de6
mohammadmehdi <mohammadmehdizamani1381@gmail.com>
2023-07-24 18:02:37 +0330

    add db_crub

---
+++ db_crud.py
@@ -0,0 +1,27 @@
+# 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
+from db_model import Record, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///mydb.db", echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base.metadata.create_all(bind=engine)
+
+
+def read_data():
+    return session.query(Record).all()
+
+
+def save_data(token, title, top_description_text, middle_description_text, bottom_description_text, image_count,
+              image_url, land_area, area, year_of_construction):
+    try:
+        record = Record(token, title, top_description_text, middle_description_text, bottom_description_text,
+                        image_count,
+                        image_url, land_area, area, year_of_construction)
+        session.add(record)
+        session.commit()
+        return True
+    except:
+        return False
