5115c996c329d7de351238ff37ddd090d53ea8a6
mohammadmehdi <mohammadmehdizamani1381@gmail.com>
2023-07-26 16:05:43 +0330

    fix bug in db_crud.py

--- db_crud.py
+++ db_crud.py
@@ -2,7 +2,7 @@ from db_model import Record, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://postgres:123456@localhost:5432/mypgdb", echo=True)
+engine = create_engine("sqlite:///mydb.db", echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
@@ -14,17 +14,19 @@ def read_records(sent_status=True, get_all=False):
         return session.query(Record).all()
     return session.query(Record).filter(Record.is_sent == sent_status).all()
 
+
 def update_record(record_token, new_state):
     record = session.query(Record).filter(Record.token == record_token).all()[0]
     record.is_sent = new_state
     session.commit()
 
+
 def save_record(token, title, top_description_text, middle_description_text, bottom_description_text, image_count,
-                image_url, land_area, area, year_of_construction):
+                image_url, land_area, area, year_of_construction, is_sent):
     try:
         record = Record(token, title, top_description_text, middle_description_text, bottom_description_text,
                         image_count,
-                        image_url, land_area, area, year_of_construction)
+                        image_url, land_area, area, year_of_construction, is_sent)
         session.add(record)
         session.commit()
         return True
