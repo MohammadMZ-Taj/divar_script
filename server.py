16819688dc50e6791eb5bbc7689669518ab8680f
mohammadmehdi <mohammadmehdizamani1381@gmail.com>
2023-07-31 14:22:01 +0330

    fix bug in post method of server

--- server.py
+++ server.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        CONFIG['house_config']['credit']['max'] = request.form['credit_max']
-        CONFIG['house_config']['credit']['min'] = request.form['credit_min']
-        CONFIG['house_config']['rent']['max'] = request.form['rent_max']
-        CONFIG['house_config']['rent']['min'] = request.form['rent_min']
+        CONFIG['house_config']['credit']['max'] = int(request.form['credit_max'])
+        CONFIG['house_config']['credit']['min'] = int(request.form['credit_min'])
+        CONFIG['house_config']['rent']['max'] = int(request.form['rent_max'])
+        CONFIG['house_config']['rent']['min'] = int(request.form['rent_min'])
         CONFIG['house_config']['rooms'] = request.form['rooms']
-        CONFIG['house_config']['size']['max'] = request.form['size_max']
-        CONFIG['house_config']['size']['min'] = request.form['size_min']
+        CONFIG['house_config']['size']['max'] = int(request.form['size_max'])
+        CONFIG['house_config']['size']['min'] = int(request.form['size_min'])
         return render_template('home.html', records=main())
     else:
         return render_template('home.html', records=read_records(get_all=True))
